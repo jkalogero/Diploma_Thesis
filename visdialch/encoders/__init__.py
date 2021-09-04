208e419d8ad2d3d9077191e94e8655f3388987f1
jkalogero <johnkalo22@hotmail.com>
2021-09-04 13:39:09 +0300

    Add minor change.

--- visdialch/encoders/__init__.py
+++ visdialch/encoders/__init__.py
@@ -1,9 +1,7 @@
-from visdialch.encoders.lf import LateFusionEncoder
-from visdialch.encoders.mn import MemoryNetworkEncoder
+from visdialch.encoders.KBGN import KBGN
 
 def Encoder(model_config, *args):
     name_enc_map = {
-        'lf': LateFusionEncoder,
-        'mn': MemoryNetworkEncoder
+        'kbgn': KBGN
     }
     return name_enc_map[model_config["encoder"]](model_config, *args)
