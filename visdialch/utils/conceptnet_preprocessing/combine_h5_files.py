795419620645ca32d542c7bd54c1a6a5884937e9
jkalogero <johnkalo22@hotmail.com>
2022-04-03 21:52:01 +0300

    Add minor changes in combining adj_list files.

--- visdialch/utils/conceptnet_preprocessing/combine_h5_files.py
+++ visdialch/utils/conceptnet_preprocessing/combine_h5_files.py
@@ -1,12 +1,12 @@
 import h5py
-
+import glob
+from tqdm import tqdm
 
 def link_file(filename, target_file):
     keys = []
     # get list of keys for current file
     with h5py.File(filename) as f:
         keys = list(f.keys())
-    print(keys)
 
     h = h5py.File(target_file, 'a')
     for key in keys:
@@ -19,12 +19,15 @@ def link_file(filename, target_file):
 
 if __name__ == '__main__':
 
-    all_files = ['file'+str(i)+'.h5' for i in range(1,10)] #change
+    all_files = glob.glob('data/train_adj_list_part_*.h5')
+    target_file = 'data/train_adj_list.h5'
 
-    for _f in all_files:
-        link_file(_f)
+    for _f in tqdm(all_files):
+        link_file(_f, target_file)
 
     # check if all keys present
-    with h5py.File('all.h5') as f:
+    with h5py.File(target_file) as f:
         all_keys = list(f.keys())
-    print(len(all_keys))
+    
+    # only for train split
+    print('All OK: ', len(all_keys) == 123287)
