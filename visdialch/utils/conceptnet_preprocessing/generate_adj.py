2f9bfb34998bfe632c844631145453169bfd48ad
jkalogero <johnkalo22@hotmail.com>
2022-05-17 10:52:39 +0300

    Add changes in preprocessing.

--- visdialch/utils/conceptnet_preprocessing/generate_adj.py
+++ visdialch/utils/conceptnet_preprocessing/generate_adj.py
@@ -9,12 +9,13 @@ from scipy.sparse import coo_matrix
 import pickle
 import h5py
 from scipy import spatial
-from collections import defaultdict
+from collections import defaultdict, Counter
 from operator import itemgetter
 
 inv_relations = ['inv_'+el for el in merged_relations]
 full_relations = merged_relations + inv_relations
-max_nodes = 45
+MAX_NODES = 45
+MAX_EDGES=100
 
 def load_resources(cpnet_vocab_path):
     global concept2id, id2concept, relation2id, id2relation
@@ -41,9 +42,9 @@ def load_cpnet(cpnet_graph_path):
 
 
 
-def createAdjList(schema_graph, adj_dict, original_concepts, max_nodes = 45,max_edges=45, num_rel=17, add_inverse=True, add_padding=True):
+def createAdjList(schema_graph, adj_dict, original_concepts, num_rel=17, add_inverse=True, add_padding=True):
     """
-    The adjacency list will have shape: (num_rel x max_nodes,max_edges), or (2xnum_rel x max_nodes,max_edges)
+    The adjacency list will have shape: (num_rel x MAX_NODES,MAX_EDGES), or (2xnum_rel x MAX_NODES,MAX_EDGES)
     if add_inverse = True.
     For each node, its list contains the index of the neighbouring node in schema_graph.
 
@@ -58,58 +59,59 @@ def createAdjList(schema_graph, adj_dict, original_concepts, max_nodes = 45,max_
     original_concepts: Set<int>
         Set of the original concepts.
 
-    max_edges: int
-        Number of relations for each node in the adjacency list
     
     add_inverse: bool
         Add inverse relations. This will convert num_rel -> 2*num_rel
     
     add_padding: bool
-        Add padding until `max_edges`. Padding value: "<PAD>".
+        Add padding until `MAX_EDGES`. Padding value: "<PAD>".
     """
     
     # initialize list
     n_rel = 2*num_rel if add_inverse else num_rel
-    adj_list = [[] for _ in range(n_rel*max_nodes)]
+    adj_list = [[] for _ in range((n_rel+1)*MAX_NODES)] # add one for the self-relation
 
+    
+    list_original_concepts = list(original_concepts)
+    # define an order for the nodes
+    indexes = {v:k for k,v in enumerate(list_original_concepts)}
+    # print(indexes)
+    # cnt=0
+    # counter = defaultdict(int)
+    for node in adj_dict: # for each node
+        if node in original_concepts: # if original
+            for r,n in adj_dict[node]: # add all of its neighbours to itself
+                idx = indexes[node] + r*MAX_NODES # compute idx
+                if len(adj_list[idx]) < MAX_EDGES:
+                    adj_list[idx].append(n) # add node
+                    # counter[r]+=1
+                    # cnt+=1
+        # add inverse
+        for r,n in adj_dict[node]: 
+            if n in original_concepts: # if original
+                idx = indexes[n] + (r+num_rel)*MAX_NODES # compute idx
+                if len(adj_list[idx]) < MAX_EDGES:
+                    adj_list[idx].append(node) # add node
+                    # cnt+=1
+                    # counter[r]+=1
+    
+    
+    for idx,node in enumerate(list_original_concepts):
+        # add self relation as a new relation
+        self_rel_id=34 # last rel id
 
-    _schema = {v:i for i,v in enumerate(schema_graph)}
-
-    # iterate schema_graph to keep the order of the nodes
-    for idx,node in enumerate(schema_graph):
-        # print('node ', id2concept[node])
-        for neighbour in adj_dict[node]:
-            # add in list only if it is original
-            if node in original_concepts:
-                i = neighbour[0]*max_nodes + idx
-                if len(adj_list[i]) < max_edges:
-                    # adj_list[i].append(str(id2concept[neighbour[1]]))
-                    adj_list[i].append(neighbour[1])
-
-            if add_inverse and neighbour[1] in original_concepts:
-                # id of inverse relation of r: r + num_rel
-                i_reverse = _schema[neighbour[1]]+(neighbour[0]+num_rel)*max_nodes
-                if i_reverse >= len(adj_list):
-                    print('\n_schema[neighbour[1]]+(neighbour[0]+num_rel)*max_nodes: ', _schema[neighbour[1]],' + (',neighbour[0],'+',num_rel,')*',max_nodes,'\n'
-                    ,'i_reverse = ', i_reverse
-                    ,'\nlen(original_concepts) = ', len(original_concepts))
-                if len(adj_list[i_reverse]) < max_edges:
-                    # adj_list[i_reverse].append(str(id2concept[node]))
-                    adj_list[i_reverse].append(node)
-        
-    for idx,node in enumerate(schema_graph):
-        # add self relation using `isa` relation type.. rel_id=5
-        if node and node in original_concepts: # if not pad
-            isa_rel_id=5
-            self_i = isa_rel_id*max_nodes + idx
-            adj_list[self_i].append(node)
-            # adj_list[self_i].append(str(id2concept[node]))
+        self_i = self_rel_id*MAX_NODES + idx
+        adj_list[self_i].append(node)
 
 
     # padding
     if add_padding:
         for idx,_ in enumerate(adj_list):
-            adj_list[idx] += [0 for _ in range(max_edges-len(adj_list[idx]))]
+            adj_list[idx] += [0 for _ in range(MAX_EDGES-len(adj_list[idx]))]
+    # print('ADJ LIST:\n')
+    # for idx,l in enumerate(adj_list):
+    #     print('node: ', idx % 45, ' relation: ', idx// 45)
+    #     print(l)
     return np.array(adj_list)
 
 
@@ -124,11 +126,21 @@ def score_triple(h,rel,t):
 def newNode(node, original_nodes, extra_nodes):
     return (node not in original_nodes) and (node not in extra_nodes)
 
-def concepts2adj(node_ids, original, limit, max_edges = 45):
+def concepts2adj(node_ids, original, limit):
     """
     Compute the adj list given a set of nodes.
     The adj list will have shape: RxN,E
+
+    Parameters:
+    ===========
+    node_ids: list
+        List of all nodes [original,neighbours]
+    original: list
+        Mask of length = len(node_ids): [True, True, ..., False, False]
+    limit: int
+        Number of original nodes and index of first False in max
     """
+
     global id2relation, relation_embs, concept_embs, threshold
 
     # print("\n\nSCHEMA GRAPH: ", len(node_ids), ' nodes') #delete
@@ -140,13 +152,13 @@ def concepts2adj(node_ids, original, limit, max_edges = 45):
     # we need a list to keep the order
     new_schema_graph = np.array(node_ids[:limit], dtype=np.int32)
     # and a set for quick checking if already visited the node
-    new_schema_graph_set = set(new_schema_graph)
+    original_nodes_set = set(new_schema_graph)
     
     
     extra_nodes = set()
     
     n_rel = len(id2relation) #number of relations
-    n_nodes = cids.shape[0] #number of nodes
+    n_nodes = cids.shape[0] #number of original nodes
     # initialize the adjacency matrix
     # adj = np.zeros((n_rel, n_nodes, n_nodes), dtype=np.uint8)
     
@@ -156,8 +168,8 @@ def concepts2adj(node_ids, original, limit, max_edges = 45):
     # min_score = 10
     # max_score = -10
     for s in range(n_nodes):
-        for t in range(n_nodes): # for each pair of nodes in initial schema graph
-            if original[s] or original[t]:
+        for t in range(n_nodes): # for each pair of nodes
+            if original[s] or original[t]: # with at least one original
                 s_c, t_c = cids[s], cids[t]
                 # print('\n\n Node: ', id2concept[s_c] , ' with ', id2concept[t_c])
                 if cpnet.has_edge(s_c, t_c): # if edge exists
@@ -168,25 +180,22 @@ def concepts2adj(node_ids, original, limit, max_edges = 45):
                             # if over the threshold or both original nodes
                             if score > threshold or (original[s] and original[t]):
                                 # skip if new node occurs but the upper limit of nodes is reached
-                                # if not ((newNode(s_c, new_schema_graph_set, extra_nodes) or newNode(t_c, new_schema_graph_set, extra_nodes)) and (len(extra_nodes) >= max_nodes - len(new_schema_graph_set))):
+                                # if not ((newNode(s_c, original_nodes_set, extra_nodes) or newNode(t_c, original_nodes_set, extra_nodes)) and (len(extra_nodes) >= MAX_NODES - len(original_nodes_set))):
                                 # if not originals add to extra nodes set
-                                if s_c not in new_schema_graph_set:
+                                if s_c not in original_nodes_set:
                                     extra_nodes.add(s_c)
-                                if t_c not in new_schema_graph_set:
+                                if t_c not in original_nodes_set:
                                     extra_nodes.add(t_c)
                                 
                                 adj_dict[s_c].append((e_attr['rel'],t_c, score))
-                                    # min_score = min(min_score, score)
-                                    # max_score = max(max_score, score)
-                                    # adj[e_attr['rel']][s][t] = 1
                                 # print(id2concept[s_c], ' --> ', id2relation[e_attr['rel']], ' --> ', id2concept[t_c], '| score = ', score)
-                                    # cnt +=1
+                                # cnt +=1
 
     # cids += 1  # note!!! index 0 is reserved for padding
-    
+    # print('cnt in adj_dict = ', cnt)
     for key,value in adj_dict.items():
         _sorted = sorted(value, key=itemgetter(2), reverse=True) # sort based on score
-        adj_dict[key] = [(el[0],el[1]) for el in _sorted[:max_edges]] # keep only the first
+        adj_dict[key] = [(el[0],el[1]) for el in _sorted[:MAX_EDGES]] # keep only the first
     
     if extra_nodes:
         new_schema_graph = np.append(new_schema_graph, list(extra_nodes))
@@ -195,9 +204,9 @@ def concepts2adj(node_ids, original, limit, max_edges = 45):
     # print("NEW SCHEMA GRAPH: ", len(new_schema_graph), ' nodes. Increase of ', len(new_schema_graph)/or_len) #delete
 
     # pad schema graph - FIRST SLICE AND PAD AND THEN CREATE ADJ LIST BASED ON THE PADDED SCHEMA GRAPH
-    # new_schema_graph = new_schema_graph[:max_nodes]
-    # new_schema_graph = np.pad(new_schema_graph, (0,max_nodes - len(new_schema_graph)))
-    adj_list = createAdjList(new_schema_graph, adj_dict, new_schema_graph_set, max_edges=max_edges)
+    # new_schema_graph = new_schema_graph[:MAX_NODES]
+    # new_schema_graph = np.pad(new_schema_graph, (0,MAX_NODES - len(new_schema_graph)))
+    adj_list = createAdjList(new_schema_graph, adj_dict, original_nodes_set)
 
     
     return adj_list, new_schema_graph
@@ -209,41 +218,35 @@ def _generateAdj(data_list):
     img_id, data = data_list
     res = []
     for _round in data: # for each round
-        if len(_round) > max_nodes:
-            # print('Exceeded on image: ', img_id)
-            print('Max nodes = ', max_nodes, 'len(_round) = ', len(_round))
-            _round = _round[len(_round)-max_nodes:] # keep the last 
-            assert len(_round) == max_nodes
-            print("Asserted that exceeding node limit was handled.")
-        all_concepts = set(_round)
-        # print('Grounded: ')
-        # for c in all_concepts:
-        #     print(id2concept[c], end='\t')
+        # keep only MAX_NODES nodes in each round
+        # These nodes will be the rows of the adj_list
+        if len(_round) > MAX_NODES:
+            # keep only the last (most recent nodes)
+            _round = _round[len(_round)-MAX_NODES:]
+
+        original_concepts = set(_round) # all original concepts
         common_neighbours = set()
-        # print("\n\nROUND CONCEPTS:") #delete
-        # for c1 in all_concepts:#delete
-        #     print(id2concept[c1])#delete
         
         # For each pair of the grounded concepts of current round
         for c1 in _round:
             for c2 in _round:
                 if c1!=c2 and c1 in cpnet_simple.nodes and c2 in cpnet_simple.nodes:
-                    # cpnet_simple[c1] will get a list of the neighbours of c1
+                    # cpnet_simple[c] will get a list of the neighbours of c
                     # common_neighbours is the intersection of the neighbours of c1 and c2
                     common_neighbours |= set(cpnet_simple[c1]) & set(cpnet_simple[c2])
     
         # Remove from the common_neighbours the grounded concepts
-        common_neighbours = common_neighbours - all_concepts
-        # Schema graph is just a set of all the nodes
-        schema_graph = sorted(all_concepts) + sorted(common_neighbours)
-        # Get the limit between original and extra nodes
-        arange = np.arange(len(schema_graph))
-        original_mask = arange < len(all_concepts)
-        adj_list, concepts = concepts2adj(schema_graph, original_mask, len(all_concepts))
+        common_neighbours = common_neighbours - original_concepts
+        # Schema graph is just a list of all the nodes
+        schema_graph = sorted(original_concepts) + sorted(common_neighbours)
+        # Get a mask for original and extra nodes. True if original.
+        original_mask = np.array([i<len(original_concepts) for i in range(len(schema_graph))])
+        # construct adjacency list
+        adj_list, concepts = concepts2adj(schema_graph, original_mask, len(original_concepts))
         
         res.append({'adj_list':adj_list, 'concepts': concepts})
-    
-    
+
+
     return (img_id, res)
     
     
@@ -263,6 +266,7 @@ def generateAdj(grounded_path, cpnet_graph_path, cpnet_vocab_path,concept_emb_pa
     print(f'Generating adjacency matrices data for {grounded_path}...\n')
     print('Will save to ', output_path)
     global concept2id, id2concept, relation2id, id2relation, cpnet_simple, cpnet
+    # concept2id and id2concept refer to the line in which each node is present in cpnet_vocab file
     load_resources(cpnet_vocab_path)
     with open(grounded_path, 'r', encoding='utf-8') as fin:
         grounded_concepts = json.load(fin)
@@ -279,10 +283,10 @@ def generateAdj(grounded_path, cpnet_graph_path, cpnet_vocab_path,concept_emb_pa
         for img_id, dialog in grounded_concepts.items()]
     
     # sample = ['378466', '332243', '378461', '287140', '575029','525211']
+    # sample = ['378466']
     # data_list = [(img_id, [[concept2id[c] for c in _round] for _round in dialog]) \
     #     for img_id, dialog in grounded_concepts.items() if img_id in sample]
     
-    # print('data_list = ', data_list)
 
     print("Will load cpnet.")
     load_cpnet(cpnet_graph_path)
@@ -296,21 +300,12 @@ def generateAdj(grounded_path, cpnet_graph_path, cpnet_vocab_path,concept_emb_pa
 
     h = h5py.File(output_path,'w')
     for k,v in tqdm(res.items()):
-        # print('Creating group for image: ', k)
         grp = h.create_group(k)
         for idx,_round in enumerate(v):
             subgrp = grp.create_group(str(idx))
-            # string_dt = h5py.special_dtype(vlen=str)
             data = np.array(_round['adj_list'], dtype=object)
-            # concept_names = np.array(_round['concepts'], dtype=object)
-            # print(type(_round['adj_list'][0]))
-            # print(type(_round['adj_list'][0][0]))
             subgrp.create_dataset('adj_list', data=data.astype(np.int64), chunks=True)
-            # subgrp.create_dataset('adj_list', data=data, dtype=string_dt)
-            # subgrp.create_dataset('concepts', data=concept_names, dtype=string_dt)
             subgrp.create_dataset('concepts', data=_round['concepts'].astype(np.int64), chunks=True)
-            # subgrp.create_dataset('original_mask', data=_round['original_mask'].astype(np.int64), chunks=True)
-            # subgrp.create_dataset('shape', data=_round['adj'].shape, chunks=True)
-        # grp.create_dataset('original_limit', data=[_round['original_limit'] for _round in v], chunks=True)
     h.close()
+
     print(f'Adj matrices saved to {output_path}.\n')
